fce6fc4971744baed39e81775c1164e86179ca9f
chenkai <kevinend51@gmail.com>
2019-05-02 22:01:10 +0800

    852. Peak Index in a Mountain Array

---
+++ 852-peak-index-in-a-mountain-array.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+
+class Solution:
+    def peakIndexInMountainArray(self, A: List[int]) -> int:
+        for i in range(len(A)):
+            if A[i] > A[i+1]:
+                return i
