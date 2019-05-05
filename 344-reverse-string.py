ec2b1eb1f79d134da85b915fa4b67abecce2d96b
chenkai <kevinend51@gmail.com>
2019-05-06 07:49:49 +0800

    344. Reverse String

---
+++ 344-reverse-string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        s.reverse()
