4049da7570fa1c7affc1dce0ef4020c5a10f9681
chenkai <kevinend51@gmail.com>
2019-05-14 06:34:17 +0800

    706. Design HashMap

---
+++ 706-design-hashmap.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+
+class MyHashMap:
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.size = 1000
+        self.table = [None] * self.size
+
+    def put(self, key: int, value: int) -> None:
+        """
+        value will always be non-negative.
+        """
+        node = self.table[key % self.size]
+        while node:
+            if node[0] == key:
+                node[1] = value
+                return
+            node = node[2]
+        self.table[key % self.size] = [key, value, self.table[key % self.size]]
+
+    def get(self, key: int) -> int:
+        """
+        Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key
+        """
+        node = self.table[key % self.size]
+        while node:
+            if node[0] == key:
+                return node[1]
+            node = node[2]
+        return -1
+
+    def remove(self, key: int) -> None:
+        """
+        Removes the mapping of the specified value key if this map contains a mapping for the key
+        """
+        prev_node, node = None, self.table[key % self.size]
+        while node:
+            if node[0] == key:
+                if prev_node:
+                    prev_node[2] = node[2]
+                else:
+                    self.table[key % self.size] = node[2]
+                return
+            prev_node = node
+            node = node[2]
+
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)
